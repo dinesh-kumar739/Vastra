87c5f4987d918b5ee1e3d03bbad0ce87a9b42eb9
Dinesh Kushwaha <dineshkumar934328@gmail.com>
2026-05-05 19:52:39 +0530

    update cors fix

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ let app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174"],
+    origin:["http://localhost:5173","https://vastra-ah4mmhxer-dinesh-kumar739.vercel.app"],
     credentials:true
 }))
 
