f48c3ec885b979028c2f74e21fbdc4bf7bd875a2
Dinesh Kushwaha <dineshkumar934328@gmail.com>
2026-05-05 20:24:24 +0530

    fixed logout cookie issue (sameSite, secure, path)

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -63,12 +63,18 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie("token")
-        return res.status(200).json({ message: "Logged out successfully" })
+        res.clearCookie("token", {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            path: "/"
+        });
+
+        return res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
-        return res.status(500).json({ message: `Logout error ${error}` })
+        return res.status(500).json({ message: `Logout error ${error}` });
     }
-}
+};
 
 export const googleLogin=async (req,res)=>{
     try {
