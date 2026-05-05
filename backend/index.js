660c2ef1c8e0a01e7fc23b53dec0f6bbfbdd8c53
Dinesh Kushwaha <dineshkumar934328@gmail.com>
2026-05-05 21:11:23 +0530

    server error solving

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ let app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173","https://vastra-drab.vercel.app"],
+    origin:["http://localhost:5173","https://vastra-drab.vercel.app","https://vastra-admin-tau.vercel.app/"],
     credentials:true
 }))
 
